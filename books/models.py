7b7b17da2cd0e4c220d0ebaaa264fa3dc1f73360
Mazzart <am.magicsmile@gmail.com>
2020-04-04 17:44:45 +0300

    Add Book model and initial migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[str(self.id)])
