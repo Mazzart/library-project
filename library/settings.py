738121c80126ee12d7480a2ac80afe740f28c8bb
Mazzart <am.magicsmile@gmail.com>
2020-04-04 18:47:18 +0300

    Add a sitemap to the project

--- library/settings.py
+++ library/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+
+    # Local
     'books.apps.BooksConfig',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
