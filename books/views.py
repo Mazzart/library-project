e8afc14fff7d9914edc82649ed46e30a264c1dc1
Mazzart <am.magicsmile@gmail.com>
2020-04-04 18:35:32 +0300

    Fix template_name field for BookList and BookDetail views

--- books/views.py
+++ books/views.py
@@ -5,9 +5,9 @@ from .models import Book
 
 class BookListView(ListView):
     model = Book
-    template_name = 'book_list.html'
+    template_name = 'books/book_list.html'
 
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'book_detail.html'
+    template_name = 'books/book_detail.html'
