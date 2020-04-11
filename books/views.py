f4c98aae1a6815d089d7edbc3baa4fded33d8739
Mazzart <am.magicsmile@gmail.com>
2020-04-11 16:01:16 +0300

    Add CreateBook view

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Book
+from .forms import BookForm
 
 
 class BookList(ListView):
@@ -11,3 +13,10 @@ class BookList(ListView):
 class BookDetail(DetailView):
     model = Book
     template_name = 'book_detail.html'
+
+
+class CreateBook(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book-list')
