bfcbc17b292cac1350c7db11039d5f539372c134
Mazzart <am.magicsmile@gmail.com>
2020-04-11 16:04:16 +0300

    Add new path and template to display form

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookList, BookDetail
+from .views import BookList, BookDetail, CreateBook
 
 urlpatterns = [
     path('', BookList.as_view(), name='book-list'),
     path('<int:pk>', BookDetail.as_view(), name='book-detail'),
+    path('create/', CreateBook.as_view(), name='add-book')
 ]
